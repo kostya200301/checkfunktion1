3add9d15fa3d6c1f081d0d23f82d7b5f3982db2b
kostya200301 <58062706+kostya200301@users.noreply.github.com>
2020-10-22 15:45:23 +0300

    Add files via upload

--- remove_palindroms.py
+++ remove_palindroms.py
@@ -1,7 +1,6 @@
-new_list = []
 def remove_palindroms(spells):
-    spells = new_list
-    for i in range(len(spells)):
+    for i2 in range(len(spells)):
         for i in spells:
-            if i.lower() == i[::-1].lower():
+            if i.replace(" ", "").lower() == i.replace(" ", "")[::-1].lower():
                 spells.remove(i)
+    return spells
